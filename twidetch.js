52a4d04f03ce5b5dab7e7941345dd0476753fdc9
Pleutre <pleutre@outlook.com>
2019-09-08 11:22:16 +0200

    Improved resize values for onclick detection

--- twidetch.js
+++ twidetch.js
@@ -1,24 +1,22 @@
 //todo add comment
 
-document.onclick= function(event) {
+document.onclick = function(event) {
     // Compensate for IE<9's non-standard event model
     //
     if (event===undefined) event= window.event;
     var target= 'target' in event? event.target : event.srcElement;
 
-	if (target.className == "player-button qa-theatre-mode-button"){
-		//alert("Changed Theater mode");		
-		if(document.querySelector("#icon_theatre_deactivate") != null) {
-			document.querySelector(".right-column--beside").style.width = "78rem";
-			document.querySelector(".persistent-player--theatre").style.width = "calc(100% - 78rem)";
-			document.querySelector(".right-column--beside .channel-root__right-column").style.width = "78rem";
-			document.querySelector(".video-player__container").style.bottom = "0rem";
-		}
-		else {
-			document.querySelector(".right-column--beside").style.width = "34rem";
-			document.querySelector(".persistent-player--theatre").style.width = "calc(100% - 34rem)";
-			document.querySelector(".right-column--beside .channel-root__right-column").style.width = "34rem";
-			document.querySelector(".video-player__container").style.bottom = "0rem";
-		}
+	//alert(document.querySelector(".video-player--theatre"));
+
+	if(document.querySelector(".video-player--theatre") != null) {
+		//alert("Mode Theater selected");
+		document.querySelector(".right-column--beside").style.width = "70.3rem";
+		document.querySelector(".persistent-player--theatre").style.width = "calc(100% - 70rem)";
+		document.querySelector(".right-column--beside .channel-root__right-column").style.width = "70.3rem";
+		document.querySelector(".video-player__container").style.bottom = "0rem";		
+	}
+	else {
+		document.querySelector(".right-column--beside").style.width = "34rem";
+		document.querySelector(".right-column--beside .channel-root__right-column").style.width = "34rem";
 	}
 };
